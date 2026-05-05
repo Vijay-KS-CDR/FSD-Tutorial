2e5f18e14a89e631b485bba124cf581abe9a70fb
vijay <ksvijay711@gmail.com>
2026-05-05 13:23:23 +0530

    Undo adding experiments

--- experiment/exp-3/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders student registration portal header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Student Registration Portal/i);
-  expect(linkElement).toBeInTheDocument();
-});
